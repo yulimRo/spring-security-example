b6fc315a305e0e053e0f77b01861656f906000de
noyulim <yyj2766@naver.com>
2024-10-06 21:06:08 +0900

    Spring Security Base Project 구성 + Custom LoginPage 적용

--- spring-sercurity-example/src/main/java/com/yoodaengs/springboot/spring_sercurity_example/config/SpringSecurityConfig.java
+++ spring-sercurity-example/src/main/java/com/yoodaengs/springboot/spring_sercurity_example/config/SpringSecurityConfig.java
@@ -1,33 +1,67 @@
 package com.yoodaengs.springboot.spring_sercurity_example.config;
 
-import javax.servlet.DispatcherType;
-
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SpringSecurityConfig {
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                //.requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(new AntPathRequestMatcher("/static/**"))
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable()
-                .authorizeHttpRequests(request -> request
-                	.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .anyRequest().authenticated()	// 어떠한 요청이라도 인증필요
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                //.cors(cors ->cors.disable())
+                //.sessionManagement()
+                //.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                //.and()
+                .authorizeHttpRequests((requests) -> requests
+                        //.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
+                        .anyRequest().authenticated()
                 )
-                .formLogin(login -> login				// form 방식 로그인 사용
-                        .defaultSuccessUrl("/", true)	// 성공 시 dashboard로
-                        .permitAll()					// 대시보드 이동이 막히면 안되므로 얘는 허용
-                )
-                .logout(Customizer.withDefaults());		// 로그아웃은 기본설정으로 (/logout으로 인증해제)
-	
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                ).httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/login"));
+
         return http.build();
     }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("ylro")
+                .password("1")
+                .roles("ADMIN")
+                .build();
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("wjchoi")
+                .password("2")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,user2);
+    }
+
 }
--- spring-sercurity-example/src/main/java/com/yoodaengs/springboot/spring_sercurity_example/controller/TestController.java
+++ spring-sercurity-example/src/main/java/com/yoodaengs/springboot/spring_sercurity_example/controller/TestController.java
@@ -1,16 +1,21 @@
 package com.yoodaengs.springboot.spring_sercurity_example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
-	
-    @ResponseBody
-    @RequestMapping(value="/")
-    public String sampleHome() {
-        return "Hello World!";
+
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
 
